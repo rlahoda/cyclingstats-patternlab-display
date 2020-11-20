aa0973dbf1a64fdebf6fc0960deb14a541ad428f
Rob Lahoda <rob@roblahoda.com>
2020-11-20 18:46:36 -0500

    updating responsive styles for a few elements

--- js/init.js
+++ js/init.js
@@ -13,20 +13,12 @@
   //Navigation toggle
 function navToggle() {
   const button = document.querySelector('#primary-nav');
-  // if contains --open remove --expanded, wait a moment then remove --open, else, add both at the same time
-  if (button.classList.contains("primary-nav--open") || button.classList.contains("primary-nav--expanded")) {
-    button.classList.remove("primary-nav--expanded")
-    setTimeout(function(){
-      button.classList.remove("primary-nav--open")
-    },200);
-    
-  } else {
-    button.classList.add("primary-nav--expanded")
-    button.classList.add("primary-nav--open")
+    button.classList.toggle("primary-nav--expanded")
+    button.classList.toggle("primary-nav--open")
     
   }
 
-}
+
 
 function navInit() {
   const button = document.querySelector('#primary-nav__menu-button');
